c9b34b401ff6665b13f4d2340322cfbe9abbea1f
jaden <ljaden.9@gmail.com>
2022-05-27 15:15:49 -0400

    created new database Schema for Tweets

---
+++ schemas/Tweet_Schema.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+// Create a tweet schema
+const Schema = mongoose.Schema
+
+const TweetSchema = new Schema({
+  content: {type: String, trim:true},
+  postedBy: {type: Schema.Types.ObjectId, ref: 'User', required:true},
+  pinned: Boolean
+
+}, {timestamps: true})
+
+
+// Model constructor
+const Tweet = mongoose.model('Tweet', TweetSchema)
+
+// export
+module.exports = Tweet;
