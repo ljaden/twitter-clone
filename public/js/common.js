5cf2e2c58f5a832ee2ebd04b095180c1eb5f521a
jaden <ljaden.9@gmail.com>
2022-05-27 21:22:40 -0400

    dynamically render tweets

--- public/js/common.js
+++ public/js/common.js
@@ -13,29 +13,76 @@ textarea.addEventListener('keyup',() => {
 
 // application/x-www-form-urlencoded
 const twt = document.getElementById('tweetForm')
-twt.addEventListener('submit',(e)=>{
+twt.addEventListener('submit',async (e)=>{
   e.preventDefault()
 
   const details = {
+    // grab users input
     content: document.getElementById('postTweetArea').value
   };
 
-let formBody = [];
-for (let property in details) {
-  const encodedKey = encodeURIComponent(property);
-  const encodedValue = encodeURIComponent(details[property]);
-  formBody.push(encodedKey + "=" + encodedValue);
-}
-// console.log(formBody) // => ['content=<tweet>']
-formBody = formBody.join("&"); // => 'content=<tweet>'
+  // formating the data
+  const encodedTweetData = (data) => {
+    let formBody = []
+    for(let property in data) {
+      const encodedKey = encodeURIComponent(property)
+      const encodedVal = encodeURIComponent(data[property])
+      // append 'content=<tweet>' to formBody arry
+      formBody.push(`${encodedKey}=${encodedVal}`) 
+    }
+    return formBody.join('&')
+  }
+  // send POST request to /api/posts -- stores tweet in db -- 
+  const rawResponse = await fetch('/api/posts', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+    },
+    body: encodedTweetData(details)
+  })
+  // returned json object from POST
+  const tweetContent = await rawResponse.json()
 
-// send tweet to server -- /api/posts 
-fetch('/api/posts', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-  },
-  body: formBody
+  const html = createTweetHtml(tweetContent)
+ 
+  const tweetsContainer = document.getElementById('tweetContainer')
+  tweetsContainer.prepend(html)
+
+  // reset tweet form
+  document.getElementById('postTweetArea').value = ''
+  // disable tweet button
+  document.getElementById('submitTweetBtn').disabled = true 
+  
 })
-.then(response => console.log(response.json())) // prints the response object
-})
+
+function createTweetHtml(data) {
+  // create container element with id 'post'
+  let divPost = document.createElement('div')
+  divPost.id = 'post'
+
+  // create container element with class 'mainContentContainer'
+  
+  let divMainContentContainer = document.createElement('div')
+  divMainContentContainer.className = 'mainContentContainer'
+  // create container with class 'userProfilePic'
+  
+  let divUserProfilePic = document.createElement('div')
+  divUserProfilePic.className = 'userProfilePic'
+
+  // ceate img element and set src attribute to user's profile picture
+  let profilePic = document.createElement('img')
+  profilePic.setAttribute(`src`,data.postedBy.profilePic)
+  
+  // 
+  divUserProfilePic.appendChild(profilePic)
+  divMainContentContainer.appendChild(divUserProfilePic)
+  
+  // create paragraph element for tweet
+  let para = document.createElement('p')
+  // let tweet = document.createTextNode(data.content)
+  para.textContent = data.content
+  divMainContentContainer.appendChild(para)
+
+  return divMainContentContainer
+}
+  // createPost(response.json())
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -13,33 +13,32 @@ app.use(express.urlencoded({extended:false}))
 router.get('/', (req,res) => {
 
 })
+
 // POST
 router.post('/',async(req,res) => {
-
   // console.log('content: ', req.body) // => {content: '<tweet>'}
   if(!req.body.content) {
     console.log('Content param not sent with request')
     return res.sendStatus(400)
   }else if(req.session.user){
-
-    // console.log(req.session.user)
-    // console.log(typeof req.session.user)
+  // console.log(req.session.user)
+  // console.log(typeof req.session.user)
     
-    // Insert to DB
-    Tweet.create({
-      content: req.body.content,
-      postedBy: req.session.user
-    })
-    .then(async(newTweet) => {
-      // add Users data to 'postedby' key
-      newTweet = await User.populate(newTweet,{path:'postedBy'})
-      res.status(201).send(newTweet)
-    })
-    .catch(error => {
-      console.log(error)
-      res.sendStatus(400)
-    })
-    // console.log(req.body.content)
+  // Insert to DB
+  Tweet.create({
+    content: req.body.content,
+    postedBy: req.session.user
+  })
+  .then(async (newTweet) => {
+    // add Users data to 'postedby' key
+    newTweet = await User.populate(newTweet,{path:'postedBy'})
+    res.status(201).send(newTweet)
+  })
+  .catch(error => {
+    console.log(error)
+    res.sendStatus(400)
+  })
+  // console.log(req.body.content)
   }else{
     res.sendStatus(400)
   }
